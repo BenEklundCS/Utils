7691eeb2ad3c9c690edac012829bcb02bfd3c75d
BenEklundCS <101027885+BenEklundCS@users.noreply.github.com>
2022-06-05 10:23:01 -0700

    Create cookieRecipe.py

---
+++ cookieRecipe.py
@@ -0,0 +1,8 @@
+sugar1 = float(1.5/48)
+butter1 = float(1/48)
+flour1 = float(2.75/48)
+desired = float(input('Enter number of cookies:'))
+neededsugar = desired*sugar1
+neededbutter = desired*butter1
+neededflour = desired*flour1
+print('You need',neededsugar,'cups of sugar,',neededbutter,'cups of butter, and', neededflour,'cups of flour.')
