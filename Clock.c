341f44e5696c9e7d56ae5f532da2ad8615ede341
Benjamin Eklund <101027885+BenEklundCS@users.noreply.github.com>
2023-09-13 20:44:41 -0700

    Create Clock.c

---
+++ Clock.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define HOURS_IN_DAY 24
+#define MINUTES_IN_HOUR 60
+
+int main() {
+    int noon = 0;
+    for(int hour = 0; hour < HOURS_IN_DAY; hour++) {
+        if(hour >= 12) {
+            noon = 1;
+        }
+        for(int minute = 0; minute < MINUTES_IN_HOUR; minute++) {
+            if(noon == 0) {
+                printf("%i : %i %s\n", hour, minute, "AM");
+            }
+            else {
+                printf("%i : %i %s\n", hour, minute, "PM");
+            }
+        }
+    }
+}
+
