5493aab6577ec987e753c84617f36f9ae50f5479
Benjamin Eklund <101027885+BenEklundCS@users.noreply.github.com>
2024-01-24 01:09:37 +0000

    add to front and end

--- C/linkedList.c
+++ C/linkedList.c
@@ -11,7 +11,35 @@ void printLinkedlist(struct node *p) {
     printf("%d ", p->data);
     p = p->next;
   }
+  printf("\n");
 }
+
+void addToEnd(struct node *p, int data) {
+  while (p->next != NULL) {
+    p = p->next;
+  }
+  
+  struct node *new;
+  new = malloc(sizeof(struct node));
+  new->data = data;
+  new->next = NULL;
+  p->next = new;
+
+}
+
+void addToFront(struct node *head, int data) {
+  struct node *new, *temp;
+  // Allocate the new node
+  new = malloc(sizeof(struct node));
+  // Move the heads data to the new node, and move the new data to the head node
+  new->data = head->data;
+  head->data = data;
+  // Connect the nodes together
+  temp = head->next;
+  head->next = new;
+  new->next = temp;
+}
+
 
 int main(int argc, char** argv) {
     struct node *head;
@@ -33,5 +61,10 @@ int main(int argc, char** argv) {
 
     head = one;
 
+    printf("\n");
+    printLinkedlist(head);
+    addToEnd(head, 10);
+    printLinkedlist(head);
+    addToFront(head, 100);
     printLinkedlist(head);
 }
